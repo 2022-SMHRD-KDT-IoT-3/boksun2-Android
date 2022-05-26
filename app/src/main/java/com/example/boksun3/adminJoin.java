7a4bcae7920f74d7c0ebf4554ec8bcf48344ddb5
smhrd <bhdm7070@naver.com>
2022-05-26 14:27:27 +0900

    관리사_회원가입완료 (2022.05.26 KMJ)

--- app/src/main/java/com/example/boksun3/adminJoin.java
+++ app/src/main/java/com/example/boksun3/adminJoin.java
@@ -1,21 +1,28 @@
 package com.example.boksun3;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DownloadManager;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 
+import android.support.v4.app.*;
+
 public class adminJoin extends AppCompatActivity {
 
-    TextView edt_date;
+    TextView edt_date , tv_address;
     EditText edt_address;
     Button btn_date, btn_address;
 
@@ -26,55 +33,122 @@ public class adminJoin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try {
+            getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+            getSupportActionBar().setCustomView(R.layout.custom_bar);
+        } catch (Exception e ){
+            System.out.println(e.getMessage());
+        }
+
         setContentView(R.layout.activity_admin_join);
 
 
         getSupportActionBar().setTitle("회원가입");
 
-       // btn_date = findViewById(R.id.btn_date);
 
-        edt_date = findViewById(R.id.edt_date);
-
-        edt_date.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(), datePicker.class);
-
-                startActivity(intent);
-
-            }
-        });
-
-        btn_address = findViewById(R.id.btn_address);
-        btn_address.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), addressSearch.class);
-
-                startActivity(intent);
-            }
-        });
-
-
-        // 주소 intent로 가져오기
-        edt_address = findViewById(R.id.edt_address);
-        Intent intent = getIntent();
-        String address = intent.getStringExtra("address");
-        edt_address.setText(address);
-
-
-        // 생년월일 intent로 가져오기
-        edt_date =findViewById(R.id.edt_date);
-        Intent intent2 = getIntent();
-        int mYear = intent2.getIntExtra("mYear",1950);
-        int mMonth = intent2.getIntExtra("mMonth",07);
-        int mDay = intent2.getIntExtra("mDay",28);
-
-        edt_date.setText(" 예 : " + mYear+ "년 " + mMonth + "월 " + mDay + "일");
+//
+//        edt_date = findViewById(R.id.edt_date);
+//
+//        edt_date.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                Intent intent = new Intent(getApplicationContext(), datePicker.class);
+//
+//                startActivity(intent);
+//
+//            }
+//        });
+//
+//        btn_address = findViewById(R.id.btn_address);
+//        btn_address.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(getApplicationContext(), addressSearch.class);
+//
+//                startActivity(intent);
+//            }
+//        });
+//
+//
+//        // 주소 intent로 가져오기
+//        edt_address = findViewById(R.id.edt_address);
+//        Intent intent = getIntent();
+//        String address = intent.getStringExtra("address");
+//        edt_address.setText(address);
+//
+//
+//
 
 
         // 복지관명 intent로 가져오기
+
+
+         Spinner s1 = findViewById(R.id.spinner1); // 도 선택 spinner
+         Spinner s2 = findViewById(R.id.spinner2); // 대전 '구' spinner
+         Spinner s3 = findViewById(R.id.spinner3); // 광주 '구' spinner
+
+         tv_address = findViewById(R.id.tv_address);
+
+
+
+
+         s1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+             @Override
+             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+
+                 if(adapterView.getSelectedItem().equals("대전광역시")){
+                     s2.setVisibility(View.VISIBLE);
+                     s2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                         @Override
+                         public void onItemSelected(AdapterView<?> adapterView2, View view, int k, long l) {
+                             tv_address.setText(adapterView.getItemAtPosition(i).toString()
+                                      + " " + adapterView2.getItemAtPosition(k).toString());
+                             s2.setVisibility(View.INVISIBLE);
+
+                         }
+
+                         @Override
+                         public void onNothingSelected(AdapterView<?> adapterView) {
+
+                         }
+                     });
+                 }
+
+                 else if(adapterView.getSelectedItem().equals("광주광역시")){
+                     s3.setVisibility(View.VISIBLE);
+                     s3.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                         @Override
+                         public void onItemSelected(AdapterView<?> adapterView3, View view, int m, long l) {
+                             tv_address.setText(adapterView.getItemAtPosition(i).toString()
+                                     + " " + adapterView3.getItemAtPosition(m).toString());
+                             s3.setVisibility(View.INVISIBLE);
+                         }
+
+                         @Override
+                         public void onNothingSelected(AdapterView<?> adapterView) {
+
+                         }
+                     });
+                 }
+
+
+
+
+
+
+
+             }
+
+             @Override
+             public void onNothingSelected(AdapterView<?> adapterView) {
+
+             }
+         });
+
+
+
 
 
 
--- app/src/main/java/com/example/boksun3/datePicker.java
+++ app/src/main/java/com/example/boksun3/datePicker.java
@@ -27,46 +27,46 @@ public class datePicker extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_date_picker);
 
-
-        tv_Date = findViewById(R.id.edt_date);
-
-        btn_ok = findViewById(R.id.btn_ok);
-
-        Calendar calendar = new GregorianCalendar();
-        mYear = calendar.get(Calendar.YEAR);
-        mMonth = calendar.get(Calendar.MONTH);
-        mDay = calendar.get(Calendar.DAY_OF_MONTH);
-
-        DatePicker datePicker = findViewById(R.id.vDatePicker);
-        datePicker.init(mYear,mMonth,mDay,mOnDateChangedListener);
+//
+//        tv_Date = findViewById(R.id.edt_date);
+//
+//        btn_ok = findViewById(R.id.btn_ok);
+//
+//        Calendar calendar = new GregorianCalendar();
+//        mYear = calendar.get(Calendar.YEAR);
+//        mMonth = calendar.get(Calendar.MONTH);
+//        mDay = calendar.get(Calendar.DAY_OF_MONTH);
+//
+//        DatePicker datePicker = findViewById(R.id.vDatePicker);
+//        datePicker.init(mYear,mMonth,mDay,mOnDateChangedListener);
 
 
     }
 
-    public void mOnClick(View v){
-        Intent intent2 = new Intent(datePicker.this,adminJoin.class);
-        intent2.putExtra("mYear",mYear);
-        intent2.putExtra("mMonth",mMonth);
-        intent2.putExtra("mDay",mDay);
-
-
-
-        startActivity(intent2);
-        finish();
-    }
-
-    DatePicker.OnDateChangedListener mOnDateChangedListener = new DatePicker.OnDateChangedListener() {
-        @Override
-        public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
-            mYear =yy;
-            mMonth = mm;
-            mDay = dd;
-
-
-
-        }
-    };
-
+//    public void mOnClick(View v){
+//        Intent intent2 = new Intent(datePicker.this,adminJoin.class);
+//        intent2.putExtra("mYear",mYear);
+//        intent2.putExtra("mMonth",mMonth);
+//        intent2.putExtra("mDay",mDay);
+//
+//
+//
+//        startActivity(intent2);
+//        finish();
+//    }
+//
+//    DatePicker.OnDateChangedListener mOnDateChangedListener = new DatePicker.OnDateChangedListener() {
+//        @Override
+//        public void onDateChanged(DatePicker datePicker, int yy, int mm, int dd) {
+//            mYear =yy;
+//            mMonth = mm;
+//            mDay = dd;
+//
+//
+//
+//        }
+//    };
+//
 
 
 
