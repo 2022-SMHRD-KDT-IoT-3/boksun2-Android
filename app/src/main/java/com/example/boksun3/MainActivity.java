2fd2ae6dc39fb0a6490367307eff5325a131fa3e
solahyun <good8low@gmail.com>
2022-06-02 15:10:45 +0900

    ss (22/06/01 SAY)

--- app/src/main/java/com/example/boksun3/MainActivity.java
+++ app/src/main/java/com/example/boksun3/MainActivity.java
@@ -2,15 +2,47 @@ package com.example.boksun3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btn3333 ;
+    Button btn_guide, btn_handiIn, btn_adminIn ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_guide = findViewById(R.id.btn_guide);
+        btn_handiIn = findViewById(R.id.btn_handiIn);
+        btn_adminIn = findViewById(R.id.btn_adminIn);
+
+        // 음성 도움말 버튼
+        btn_guide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        // 장애인 버튼
+        btn_handiIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), hanidJoinNFC.class);
+                startActivity(intent);
+            }
+        });
+
+        // 복지사 버튼
+        btn_adminIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), adminLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
