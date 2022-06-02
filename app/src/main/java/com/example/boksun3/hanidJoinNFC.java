0f6e3f30be8130e6aa3b1e5089121a8816d66b39
smhrd <bhdm7070@naver.com>
2022-06-02 11:24:24 +0900

    handiJoinNFC (김민정)

---
+++ app/src/main/java/com/example/boksun3/hanidJoinNFC.java
@@ -0,0 +1,14 @@
+package com.example.boksun3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hanidJoinNFC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hanid_join_nfc);
+    }
+}
